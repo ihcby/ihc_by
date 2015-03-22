7c93f70392de88e6843798b4cc12d384d1b82bb5
Denis Karpenko <denis@karpenko.me>
2015-03-22 18:28:19 +0300

    Increase picture size

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,7 @@ class Picture < ActiveRecord::Base
   belongs_to :trial
   validates_presence_of :trial
 
-  has_attached_file :image, styles: { large: '800x800>', medium: '300x300#', thumb: '150x150#', thumb_small: '30x30#' }, default_url: '/images/:style/missing.png'
+  has_attached_file :image, styles: { large: '1200x1200>', medium: '600x600#', thumb: '150x150#', thumb_small: '30x30#' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
